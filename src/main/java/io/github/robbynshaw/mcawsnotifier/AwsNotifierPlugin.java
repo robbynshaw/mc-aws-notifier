20671b508dc6dd484d688e6cd854b354cd4e721b
Rob Shaw <robbynshaw@gmail.com>
2024-09-28 16:12:25 +0000

    feat: added aws client

--- src/main/java/io/github/robbynshaw/mcawsnotifier/AwsNotifierPlugin.java
+++ src/main/java/io/github/robbynshaw/mcawsnotifier/AwsNotifierPlugin.java
@@ -12,16 +12,21 @@ import org.bukkit.plugin.java.JavaPlugin;
 
 import lombok.Getter;
 import lombok.experimental.Accessors;
-// import software.amazon.awssdk.services.ecs.model.UpdateServiceRequest;
+import software.amazon.awssdk.regions.Region;
+import software.amazon.awssdk.services.ecs.EcsClient;
+import software.amazon.awssdk.services.ecs.model.UpdateServiceRequest;
 
 public class AwsNotifierPlugin extends JavaPlugin implements Listener {
     private boolean worldIsActive = false;
     private boolean timerIsRunning = false;
     private final Timer timer;
-    private int checkInterval;
-    private final int defaultCheckMins = 1;
-    private int checkMins;
+    private int shutdownTimeoutMS;
+    private final int defaultTimeoutMins = 1;
+    private final int timeoutMins;
     private int activeUsers = 0;
+
+    private final String CLUSTER_NAME;
+    private final String SERVICE_ARN;
 
     @Getter
     @Accessors(fluent = true)
@@ -31,13 +36,11 @@ public class AwsNotifierPlugin extends JavaPlugin implements Listener {
         instance = this;
         timer = new Timer();
 
-        try {
-            String checkVal = System.getenv("ACTIVE_PLAYER_CHECK_INTERVAL_MINUTES");
-            checkMins = Integer.parseInt(checkVal);
-        } catch (NumberFormatException e) {
-            checkMins = defaultCheckMins;
-        }
-        checkInterval = 1000 * 60 * checkMins;
+        timeoutMins = AwsNotifierPlugin.GetEnvInt("SHUTDOWN_TIMEOUT_MINUTES", defaultTimeoutMins);
+        shutdownTimeoutMS = 1000 * 60 * timeoutMins;
+
+        CLUSTER_NAME = System.getenv("CLUSTER_NAME");
+        SERVICE_ARN = System.getenv("SERVICE_ARN");
     }
 
     @Override
@@ -72,18 +75,6 @@ public class AwsNotifierPlugin extends JavaPlugin implements Listener {
         }
     }
 
-    private void zeroService() {
-        // UpdateServiceRequest updateServiceRequest = UpdateServiceRequest.builder()
-        // .cluster(clusterName)
-        // .service(serviceName)
-        // .desiredCount(0) // Set desired instances to zero
-        // .build();
-
-        // // Update the service
-        // Service updatedService =
-        // ecsClient.updateService(updateServiceRequest).service();
-    }
-
     private void cancelTimer() {
         try {
             if (timerIsRunning) {
@@ -95,17 +86,32 @@ public class AwsNotifierPlugin extends JavaPlugin implements Listener {
     }
 
     private void startNewTimeout() {
-        getLogger().info("Scheduling an active player check in " + checkMins + " minute(s)...");
+        getLogger().info("Scheduling an active player check in " + shutdownTimeoutMS + " minute(s)...");
         cancelTimer();
-        timer.schedule(new CheckUsersTask(), checkInterval);
+        timer.schedule(new CheckUsersTask(), shutdownTimeoutMS);
         timerIsRunning = true;
     }
 
     private void checkForActiveUsers() {
         getLogger().info("Checking active players: " + activeUsers + " found.");
         if (activeUsers == 0) {
-            getLogger().info("No active users for the last " + checkMins + " minute(s). Shutting down...");
+            getLogger().info("No active users for the last " + timeoutMins + " minute(s). Shutting down...");
+            zeroService();
         }
+    }
+
+    private void zeroService() {
+        EcsClient ecsClient = EcsClient.builder().region(Region.US_EAST_1).build();
+
+        UpdateServiceRequest updateServiceRequest = UpdateServiceRequest.builder()
+                .cluster(CLUSTER_NAME)
+                .service(SERVICE_ARN)
+                .desiredCount(0) // Set desired instances to zero
+                .build();
+
+        ecsClient.updateService(updateServiceRequest).service();
+
+        ecsClient.close();
     }
 
     private class CheckUsersTask extends TimerTask {
@@ -113,6 +119,15 @@ public class AwsNotifierPlugin extends JavaPlugin implements Listener {
         public void run() {
             checkForActiveUsers();
             timerIsRunning = false;
+        }
+    }
+
+    private static int GetEnvInt(String key, int defaultValue) {
+        try {
+            String checkVal = System.getenv(key);
+            return Integer.parseInt(checkVal);
+        } catch (NumberFormatException e) {
+            return defaultValue;
         }
     }
 }
